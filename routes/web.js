9503a4deab5e12c0615162ad8c9ac5d8983220f6
ritikraghuwanshi <ritikraghuwanshi987@gmail.com>
2023-04-15 13:02:15 +0530

    raghuwanshi

--- routes/web.js
+++ routes/web.js
@@ -29,38 +29,38 @@ router.get('/register',FrontController.register)
 
 //admin controller
 router.get('/admin/dashboard',auth,AdminController.dashboard)
-router.post('/adminregister',AdminController.register)
-router.post('/verifylogin',AdminController.verifylogin)
-router.get('/logout',AdminController.logout)
+router.post('/adminregister',auth,AdminController.register)
+router.post('/verifylogin',auth,AdminController.verifylogin)
+router.get('/logout',auth,AdminController.logout)
 
 
 //blog controller
 router.get('/admin/Blogdisplay',auth,BlogController.displayBlog)
-router.post('/insertBlog',BlogController.insertblog)
-router.get('/Blogview/:id',BlogController.blogview)
-router.get('/admin/blogedit/:id',BlogController.blogEdit)
-router.post('/Blogupdate/:id',BlogController.blogUpdate)
-router.get('/admin/Blogdelete/:id',BlogController.blogdelete)
+router.post('/insertBlog',auth,BlogController.insertblog)
+router.get('/Blogview/:id',auth,BlogController.blogview)
+router.get('/admin/blogedit/:id',auth,BlogController.blogEdit)
+router.post('/Blogupdate/:id',auth,BlogController.blogUpdate)
+router.get('/admin/Blogdelete/:id',auth,BlogController.blogdelete)
 
 //categories controller
 router.get('/admin/Categoriesdisplay',auth,CategoriesController.displayCategories)
-router.post('/insertCategories',CategoriesController.insertcategories)
-router.get('/Categoriesview/:id',CategoriesController.categoriesview)
-router.get('/admin/Categoriesedit/:id',CategoriesController.categoriesEdit)
-router.post('/Categoriesupdate/:id',CategoriesController.categoriesUpdate)
-router.get('/admin/Categoriesdelete/:id',CategoriesController.categoriesdelete)
+router.post('/insertCategories',auth,CategoriesController.insertcategories)
+router.get('/Categoriesview/:id',auth,CategoriesController.categoriesview)
+router.get('/admin/Categoriesedit/:id',auth,CategoriesController.categoriesEdit)
+router.post('/Categoriesupdate/:id',auth,CategoriesController.categoriesUpdate)
+router.get('/admin/Categoriesdelete/:id',auth,CategoriesController.categoriesdelete)
 
 
 //About Controller
 router.get('/admin/Aboutdisplay',auth,AboutController.displayabout)
-router.post('/insertAbout',AboutController.insertabout)
-router.get('/admin/Aboutedit/:id',AboutController.aboutEdit)
-router.post('/Aboutupdate/:id',AboutController.aboutUpdate)
-router.get('/admin/Aboutdelete/:id',AboutController.aboutdelete)
+router.post('/insertAbout',auth,AboutController.insertabout)
+router.get('/admin/Aboutedit/:id',auth,AboutController.aboutEdit)
+router.post('/Aboutupdate/:id',auth,AboutController.aboutUpdate)
+router.get('/admin/Aboutdelete/:id',auth,AboutController.aboutdelete)
 
 //contact controller
 router.get('/admin/Contactdisplay',auth,ContactController.displaycontact)
-router.post('/insertContact',ContactController.contactInsert)
+router.post('/insertContact',auth,ContactController.contactInsert)
 
 //logout controller
 router.get('/admin/logutdisplay',LogoutController.displaylogout)
